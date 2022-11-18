2c8a30ba708d277f9f68b8577bcc7a9223fcaf35
Bruna Baudel <brubaudel@gmail.com>
2022-11-18 12:15:23 +0100

    Initial Commit

---
+++ Place/PlaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaceApp.swift
+//  Place
+//
+//  Created by Bruna Baudel on 18/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct PlaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
